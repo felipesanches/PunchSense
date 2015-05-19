7545cbc2e00848f987debb8399055803cb1135e8
Felipe Correa da Silva Sanches <juca@members.fsf.org>
2015-05-19 20:47:35 +0000

    Adding licensing info to the header of the main python script

--- punchsense.py
+++ punchsense.py
@@ -1,3 +1,26 @@
+##########################################################
+# == PunchSense ==
+#
+# Hardware + Software to detect punches to a boxing
+# punch bag and to make fun stuff with the collected data.
+#
+# == Licensing terms ==
+#
+# (c)2015 Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the
+# GNU Lesser General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License along with this program.
+# If not, see <https://www.gnu.org/copyleft/lesser.html>.
+#
+##########################################################
+
 play_samples_at_every_hit = False
 tolerable_error = 500 #msec
 main_theme_start_time = (60*1 + 45)
